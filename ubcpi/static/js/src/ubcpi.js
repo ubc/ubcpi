8e03931c1eb83f64978d880069b7240209ff4b5b
Pan Luo <pan.luo@ubc.ca>
2015-03-02 12:03:01 -0800

    Remove unused js for savedAnswer

--- ubcpi/static/js/src/ubcpi.js
+++ ubcpi/static/js/src/ubcpi.js
@@ -15,25 +15,6 @@ function PeerInstructionXBlock(runtime, element, data) {
     "use strict";
 
     var handlerUrl = runtime.handlerUrl(element, 'submit_answer');
-    var submitButton = $('.ubcpi_submit', element);
-
-/*
-    $('input[type="radio"]', element).click(function (eventObject) {
-        var clicked = this;
-        var answer = clicked.value;
-        console.log("Submitting ", answer);
-        $.ajax({
-            type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify({"q": answer}),
-            success: function (data, textStatus, jqXHR) {
-                disableSubmit();
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-            }
-        });
-    });
-*/
 
     $(function ($) {
         var appId = generatePIXBlockId();
@@ -71,22 +52,7 @@ function PeerInstructionXBlock(runtime, element, data) {
                     }
                 });
             };
-
         });
         angular.bootstrap(element, [appId]);
-
-        /* Here's where you'd do things on page load. */
-        var savedAnswer = data.answer;
-
-        // Handle null case
-        if (!savedAnswer) {
-            return;
-        }
-        else {
-            enableSubmit();
-        }
-
-        $('input[value="' + savedAnswer + '"]', element).prop('checked', 'checked');
-
     });
 }
