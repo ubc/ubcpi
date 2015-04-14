d250fa543792b3996edf9a08fd0a65c911b569a0
Richard Tape <richard.tape@ubc.ca>
2015-04-14 10:47:22 -0700

    CHANGE to use spaces not tabs ADD correct_rationale to edit model

--- ubcpi/static/js/src/ubcpi_edit.js
+++ ubcpi/static/js/src/ubcpi_edit.js
@@ -35,49 +35,51 @@ function PIEdit(runtime, element, data) {
         app.controller('EditSettingsController', function ($scope, $http) {
             var self = this;
             self.algos = data.algos;
-			self.data = {};
-			self.data.display_name = data.display_name;
+            self.data = {};
+            self.data.display_name = data.display_name;
             self.data.question_text = data.question_text;
-			self.data.options = data.options;
-			self.data.correct_answer = data.options[0];
-			if (data.correct_answer)
-				self.data.correct_answer = data.correct_answer;
+            self.data.options = data.options;
+            self.data.correct_answer = data.options[0];
+            if (data.correct_answer)
+                self.data.correct_answer = data.correct_answer;
+            if (data.correct_rationale)
+                self.data.correct_rationale = data.correct_rationale;
             self.data.algo = data.algo;
             self.data.seeds = data.seeds;
 
-			self.cancel = function() {
-				runtime.notify('cancel', {});
-			};
-			self.add_option = function() {
-				self.data.options.push('');
-			};
-			self.delete_option = function(index) {
-				self.data.options.splice(index, 1);
-			};
+            self.cancel = function() {
+                runtime.notify('cancel', {});
+            };
+            self.add_option = function() {
+                self.data.options.push('');
+            };
+            self.delete_option = function(index) {
+                self.data.options.splice(index, 1);
+            };
             self.addSeed = function() {
                 self.data.seeds.push({});
             };
             self.deleteSeed = function(index) {
                 self.data.seeds.splice(index, 1);
             };
-			self.submit = function() {
-				// Take all of the fields, serialize them, and pass them to the
-				// server for saving.
-				var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
+            self.submit = function() {
+                // Take all of the fields, serialize them, and pass them to the
+                // server for saving.
+                var handlerUrl = runtime.handlerUrl(element, 'studio_submit');
 
-				runtime.notify('save', {state: 'start', message: "Saving"});
-				
+                runtime.notify('save', {state: 'start', message: "Saving"});
+                
                 $http.post(handlerUrl, self.data).
                     success(function(data, status, header, config) {
-						runtime.notify('save', {state: 'end'})
+                        runtime.notify('save', {state: 'end'})
                     }).
                     error(function(data, status, header, config) {
-						runtime.notify('error', {
-							'title': 'Error saving question',
-							'message': self.format_errors(result['errors'])
-						});
+                        runtime.notify('error', {
+                            'title': 'Error saving question',
+                            'message': self.format_errors(result['errors'])
+                        });
                     });
-			};
+            };
         });
         angular.bootstrap(element, ["ubcpi_edit"]);
     });
