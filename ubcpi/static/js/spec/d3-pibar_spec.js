330aeb76681d2aa534d6a83f566c60e4bd9a2faa
Bill Pickard <bill.r.pickard@gmail.com>
2016-06-07 13:33:19 -0700

    Added unit tests

--- ubcpi/static/js/spec/d3-pibar_spec.js
+++ ubcpi/static/js/spec/d3-pibar_spec.js
@@ -4,41 +4,95 @@ describe('D3 bar chart', function () {
     var chart;
     var chartContainer;
 
-    beforeEach(function() {
-        var scope = {role:'student'};
-        chart = d3.custom.barChart(scope);
-        chartContainer = d3.select('body')
-            .append('div')
-            .attr('class', 'testContainer');
+    describe('instructor view', function(){
+
+        beforeEach(function() {
+            var scope = {role:'instructor'};
+            chart = d3.custom.barChart(scope);
+            chartContainer = d3.select('body')
+                .append('div')
+                .attr('class', 'testContainer');
+        });
+
+        afterEach(function() {
+            // clean up
+            chartContainer.remove();
+        });
+
+        it('should provide getters and setters', function() {
+            var defaultChartWidth  = chart.width();
+            var defaultChartHeight = chart.height();
+            var defaultMinTotalFrequency = chart.minTotalFrequency();
+
+            chart.width(100)
+                .height(50)
+                .minTotalFrequency(20);
+
+            var newChartWidth  = chart.width();
+            var newChartHeight = chart.height();
+            var newMinTotalFrequency = chart.minTotalFrequency();
+
+
+            expect(defaultChartWidth).not.toBe(100);
+            expect(defaultChartHeight).not.toBe(50);
+            expect(defaultMinTotalFrequency).not.toBe(20);
+            expect(newChartWidth).toBe(100);
+            expect(newChartHeight).toBe(50);
+            expect(newMinTotalFrequency).toBe(20);
+        });
+
+
+        it('should have a minTotalFrequency of 1', function(){
+            var defaultMinTotalFrequency = chart.minTotalFrequency();
+            expect(defaultMinTotalFrequency).toBe(1);
+        });
+    });
+    
+      describe('student view', function(){
+
+        beforeEach(function() {
+            var scope = {role:'student'};
+            chart = d3.custom.barChart(scope);
+            chartContainer = d3.select('body')
+                .append('div')
+                .attr('class', 'testContainer');
+        });
+
+        afterEach(function() {
+            // clean up
+            chartContainer.remove();
+        });
+
+        it('should provide getters and setters', function() {
+            var defaultChartWidth  = chart.width();
+            var defaultChartHeight = chart.height();
+            var defaultMinTotalFrequency = chart.minTotalFrequency();
+
+            chart.width(100)
+                .height(50)
+                .minTotalFrequency(20);
+
+            var newChartWidth  = chart.width();
+            var newChartHeight = chart.height();
+            var newMinTotalFrequency = chart.minTotalFrequency();
+
+
+            expect(defaultChartWidth).not.toBe(100);
+            expect(defaultChartHeight).not.toBe(50);
+            expect(defaultMinTotalFrequency).not.toBe(20);
+            expect(newChartWidth).toBe(100);
+            expect(newChartHeight).toBe(50);
+            expect(newMinTotalFrequency).toBe(20);
+        });
+
+        it('should have a minTotalFrequency of 10', function(){
+            var defaultMinTotalFrequency = chart.minTotalFrequency();
+            expect(defaultMinTotalFrequency).toBe(10);
+        });
     });
 
-    afterEach(function() {
-        // clean up
-        chartContainer.remove();
-    });
 
-    it('should provide getters and setters', function() {
-        var defaultChartWidth  = chart.width();
-        var defaultChartHeight = chart.height();
-        var defaultMinTotalFrequency = chart.minTotalFrequency();
-
-        chart.width(100)
-            .height(50)
-            .minTotalFrequency(20);
-
-        var newChartWidth  = chart.width();
-        var newChartHeight = chart.height();
-        var newMinTotalFrequency = chart.minTotalFrequency();
-
-
-        expect(defaultChartWidth).not.toBe(100);
-        expect(defaultChartHeight).not.toBe(50);
-        expect(defaultMinTotalFrequency).not.toBe(20);
-        expect(newChartWidth).toBe(100);
-        expect(newChartHeight).toBe(50);
-        expect(newMinTotalFrequency).toBe(20);
-    });
-
+    
     //it('should render a chart with minimal requirements', function() {
     //
     //    chartContainer.datum(dataset)
