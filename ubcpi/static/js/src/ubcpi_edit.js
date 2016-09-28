48d7f4f41a382e9080828176683ecb37ab373c73
Justin Leong <justin.leong@ubc.ca>
2016-09-28 13:16:19 -0700

    Fix rationale bug on select of no correct answer
    The rationale text was not correctly set when "No correct answer" was
    selected for the first setting of a correct answer for a question.

--- ubcpi/static/js/src/ubcpi_edit.js
+++ ubcpi/static/js/src/ubcpi_edit.js
@@ -145,7 +145,7 @@ angular.module("ubcpi_edit", ['ngMessages', 'ngSanitize', 'ngCookies'])
 
             self.submit = function() {
                 notify('save', {state: 'start', message: "Saving"});
-                if(data.correct_answer == data.options.length)
+                if(self.data.correct_answer == data.options.length)
                     self.data.correct_rationale.text = "n/a";
 
                 return studioBackendService.submit(self.data).catch(function(errors) {
