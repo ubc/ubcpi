8770d2ef5b62fc997f66a6e8f1bb7dc2655a8d4f
Luis Moreno <luis.moreno@edunext.co>
2019-10-11 16:45:58 -0400

    Use locale name instead of language name

--- ubcpi/utils.py
+++ ubcpi/utils.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 #
 
+from django.utils import translation
 
 # Make '_' a no-op so we can scrape strings
 def _(text):
     return text
+
+def get_language():
+    """
+    Return the locale name of the user language.
+    """
+    return translation.to_locale(translation.get_language())
